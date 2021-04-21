9e2fd99872768742cafcc2fa757bcbc3266ec534
AndresMartin510 <77711438+AndresMartin510@users.noreply.github.com>
2021-04-20 20:50:38 -0700

    added new ProgramDirectors model to branch (#6)

---
+++ migrations/20210421034313-create-events.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ProgramDirectors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      directorType: {
+        type: Sequelize.STRING
+      },
+      userOrgRole: {
+        type: Sequelize.STRING
+      },
+      OrgId: {
+        type: Sequelize.INTEGER
+      },
+      usersId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ProgramDirectors');
+  }
+};
