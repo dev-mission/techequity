bd9283eae99b1413c1a2bcd653a1776e458c0c16
César Castro <emanuel@minerva.kgi.edu>
2021-05-05 20:18:53 -0700

    💄 updating profile setting form

--- client/src/Settings.js
+++ client/src/Settings.js
@@ -24,21 +24,21 @@ function Settings() {
                               <br></br>
                               <div><span class="text-danger">* </span>
                           <span> Organization Name</span><br></br>
-                          <input type="text" id="fname" name="fname"></input></div>
+                          <input type="text" class="form-control" id="orgname" name="orgname" placeholder="Acme"></input></div>
                           
                           <br>
                           </br>
 
                           <div><span class="text-danger">* </span>
                           <span> Organization Type</span><br></br>
-                          <input type="text" id="fname" name="fname"></input></div>
+                          <input type="text" class="form-control"  id="orgtype" name="orgtype" placeholder="Select organization type"></input></div>
                           
                           <br>
                           </br>
 
                           <div><span class="text-danger">* </span>
                           <span> Organization ID Number</span><br></br>
-                          <input type="text" id="fname" name="fname"></input></div>
+                          <input type="text" class="form-control"  id="orgid" name="orgid" placeholder="123-456-789 e.g."></input></div>
                           
                           <br>
                           </br>
@@ -57,35 +57,35 @@ function Settings() {
 
                               <div><span class="text-danger">* </span>
                           <span> Organization Email</span><br></br>
-                          <input type="text" id="fname" name="fname"></input></div>
+                          <input type="text" class="form-control"  id="orgemail" name="orgemail" placeholder="name@company.com"></input></div>
                           
                           <br>
                           </br>
 
                           <div><span class="text-danger">* </span>
                           <span> Address</span><br></br>
-                          <input type="text" id="fname" name="fname"></input></div>
+                          <input type="text" class="form-control"  id="orgaddress" name="orgaddress" placeholder="123 Main St. e.g."></input></div>
                           
                           <br>
                           </br>
 
                           <div><span class="text-danger">* </span>
                           <span> Country</span><br></br>
-                          <input type="text" id="fname" name="fname"></input></div>
+                          <input type="text" class="form-control"  id="country" name="country" placeholder="Select country"></input></div>
                           
                           <br>
                           </br>
 
                           <div><span class="text-danger">* </span>
                           <span> State</span><br></br>
-                          <input type="text" id="fname" name="fname"></input></div>
+                          <input type="text" class="form-control"  id="fname" name="fname" placeholder="Select state"></input></div>
                           
                           <br>
                           </br>
 
                           <div><span class="text-danger">* </span>
                           <span> City</span><br></br>
-                          <input type="text" id="fname" name="fname"></input></div>
+                          <input type="text" class="form-control" id="fname" name="fname" placeholder="Select city"></input></div>
                           
                           <br>
                           </br>
@@ -101,13 +101,13 @@ function Settings() {
                               <br></br>
 
                               <div><span> Please briefly describe your organization</span><br></br>
-                          <input type="text" id="fname" name="fname"></input></div>
+                              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Brief description."></textarea></div>
                           
                           <br>
                           </br>
 
                           <div><span> Why do you want to partner with FairPlay?</span><br></br>
-                          <input type="textarea" id="fname" name="fname"></input></div>
+                          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Brief description."></textarea></div>
                           
                           <br>
                           </br>
