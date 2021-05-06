f5f671b02537f784ab3e1c8f4769b3bbadb5ee41
César Castro <emanuel@minerva.kgi.edu>
2021-05-05 18:04:36 -0700

    💄 UI update users can see the settings of profile

--- client/src/Header.js
+++ client/src/Header.js
@@ -29,20 +29,29 @@ function Header() {
   return (
     <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top">
       <div className="container">
-        <Link className="navbar-brand" to="/">Tech Equity</Link>
+        <Link className="navbar-brand" to="/">FairPlay</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>    
         <div className="collapse navbar-collapse" id="navbarsExampleDefault">
           <ul className="navbar-nav flex-grow-1 mb-2 mb-md-0">
             <li className="nav-item active">
-              <Link className="nav-link" aria-current="page" to="/">Tech Equity</Link>
+              <Link className="nav-link" aria-current="page" to="/">Home</Link>
             </li>
             <div className="flex-grow-1 d-flex justify-content-end">
+            {user && (
+              <li className="nav-item active">
+                  <Link className="nav-link" aria-current="page" to="/">Settings</Link>
+              </li>
+              
+            )}
               {user && (
+                
                 <li className="nav-item">
-                  <a className="nav-link" href="/logout" onClick={onLogout}>Log out</a>
-                </li>)}
+                  <a class="btn btn-primary" role="button" href="/logout" onClick={onLogout}>Log out</a>
+                  
+                </li>
+                )}
               {!user && (
                 <li className="nav-item">
                   <Link className="nav-link" to="/login">Log in</Link>
