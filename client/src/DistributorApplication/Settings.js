d106c6cdfd4d87351a791dae47c3d6ccff913665
cesar-ca <emanuel@minerva.kgi.edu>
2021-05-19 02:23:23 -0700

    💄 working CRUD for finish set up profile

--- client/src/DistributorApplication/Settings.js
+++ client/src/DistributorApplication/Settings.js
@@ -46,150 +46,110 @@ function Settings() {
         }
     }
     return (
-        <main className="container">
-          <div className="row">
-            <div className="col-md-9 offset-md-2">
+      <main className="container">
+          <h1>Welcome, Sophie!</h1>
+          <p>Let's finish setting up your distributor profile</p>
+
+          <div class="p-3 mb-2 bg-light text-dark rounded" id="roundedBox">
               <br />
+              <p class="text-danger">* Indicates a required response.</p>
               <br />
-              <h1>Welcome, Sophie!</h1>
-              <p>Let's finish setting up your distributor profile</p>
-              <br />
-              <div class="p-3 mb-2 bg-light text-dark rounded" id="roundedBox">
-                <br />
-        
-                <p class="text-danger">* Indicates a required response.</p>
-                <br />
-                <div className="row">
+
+
+
+          <form onSubmit={onSubmit}>
+              <div className="row">
                   <div className="col-md-2">
-                    <br />
-                    Admin
+                      <br />
+                      Admin
                   </div>
+
                   <div className="col-md-10">
-                    <br />
-                    <div>
+                      <br />
+                      <div>
                       <span class="text-danger">* </span> <span> <strong>Organization Name</strong></span> <br />
-                      <input className="form-control" type="text" name="orgname" placeholder="Acme" value={director_profile.orgName} onChange={onChange} />
-                    </div>
-                    <br />
-                    <div>
+                      <input className="form-control" type="text" name="orgName" placeholder="Acme" value={director_profile.orgName} onChange={onChange} />
+                      </div>
+                      <br />
+                      <div>
                       <span class="text-danger">* </span> <span> <strong>Organization Type </strong> </span ><br />
-                      <input className="form-control" type="text" name="orgtype" placeholder="Select organization type" value={director_profile.orgType} onChange={onChange} />
-                    </div>
-                    <br />
-                    <div>
-                      <span class="text-danger">* </span>
-                      <span> <strong> Organization ID Number  </strong> </span><br />
-                      <input className="form-control" type="text" name="orgid" placeholder="123-456-789 e.g." value={director_profile.orgID} onChange={onChange}/>
-                    </div>
-                    <br />
+                      <input className="form-control" type="text" name="orgType" placeholder="Select organization type" value={director_profile.orgType} onChange={onChange} />
+                      </div>
+                      <br />
+                      <div>
+                      <span class="text-danger">* </span><span> <strong> Organization ID Number  </strong> </span><br />
+                      <input className="form-control" type="text" name="orgID" placeholder="123-456-789 e.g." value={director_profile.orgID} onChange={onChange}/>
+                      </div>
+                      <br />
                   </div>
                   <br />
                   <br />
                   <div className="col-md-2">
-                    <br />
-                    <br />
-                    Contact
+                      <br />
+                      <br />
+                      Contact
                   </div>
                   <div className="col-md-10">
-                    <br />
-                    <br />
-        
-                    <div>
-                      <span class="text-danger">* </span>
-                      <span> <strong> Organization Email </strong></span><br />
-                      <input type="text" class="form-control" id="orgemail" name="orgemail" placeholder="name@company.com" />
-                    </div>
-        
-                    <br />
-        
-                    <div>
+                      <br />
+                      <br />
+                      <div>
+                      <span class="text-danger">* </span><span> <strong> Organization Email </strong></span><br />
+                      <input className="form-control" type="text" name="orgEmail" placeholder="name@company.com" value={director_profile.orgEmail} onChange={onChange}/>
+                      </div>
+                      <br />
+                      <div>
                       <span class="text-danger">* </span> <span> <strong>Address </strong> </span><br />
-                      <input type="text" class="form-control" id="orgaddress" name="orgaddress" placeholder="123 Main St. e.g."
-                      />
-                    </div>
-        
-                    <br />
-        
-                    <div>
+                      <input className="form-control" type="text" name="address" placeholder="123 Main St. e.g."  value={director_profile.address} onChange={onChange} />
+                      </div>
+                      <br />
+                      <div>
                       <span class="text-danger">* </span> <span> <strong> Country </strong> </span><br />
-                      <input type="text" class="form-control" id="country" name="country" placeholder="Select country" />
-                    </div>
-        
-                    <br />
-        
-                    <div>
+                      <input className="form-control" type="text" name="country" placeholder="Select country" value={director_profile.country} onChange={onChange} />
+                      </div>
+                      <br />
+                      <div>
                       <span class="text-danger">* </span> <span> <strong> State </strong> </span><br />
-                      <input type="text" class="form-control" id="state" name="state" placeholder="Select state" />
-                    </div>
-        
-                    <br />
-        
-                    <div>
+                      <input className="form-control" type="text" name="state" placeholder="Select state" value={director_profile.state} onChange={onChange} />
+                      </div>
+                      <br />
+                      <div>
                       <span class="text-danger">* </span> <span> <strong> City </strong> </span><br />
-                      <input type="text" class="form-control" id="city" name="city" placeholder="Select city" />
-                    </div>
-        
-                    <br />
+                      <input className="form-control" type="text" name="city" placeholder="Select city" value={director_profile.city} onChange={onChange} />
+                      </div>
+                      <br />
                   </div>
                   <div className="col-md-2">
-                    <br />
-        
-                    <br />
-                    Profile
+                      <br />
+                      <br />
+                      Profile
                   </div>
                   <div className="col-md-10">
-                    <br />
-                    <br />
-        
-                    <div>
-                      <span> <strong> Please briefly describe your organization </strong> </span><br />
-                      <textarea
-                        class="form-control"
-                        id="describeOrg"
-                        rows="3"
-                        placeholder="Brief description."
-                      ></textarea>
-                    </div>
-        
-                    <br />
-        
-                    <div>
-                      <span> <strong> Why do you want to partner with FairPlay? </strong> </span><br />
-                      <textarea
-                        class="form-control"
-                        id="describePartner"
-                        rows="3"
-                        placeholder="Brief description."
-                      ></textarea>
-                    
-                     
-                    </div>
-                    
-                    <br />
+                  <br />
+                  <br />
+                  <div>
+                    <span> <strong> Please briefly describe your organization </strong> </span><br />
+                    <textarea className="form-control" name="describeOrg" rows="3" placeholder="Brief description." value={director_profile.describeOrg} onChange={onChange}></textarea>
                   </div>
                   <br />
-                  <hr></hr>
-                  <div class="row">
-                  <div className="col-md-3 mb-2">
-                    <a href="#"style={{color:"#1154FF", textDecoration:"none"}}>Return To Sign-Up</a>
+                  <div>
+                    <span> <strong> Why do you want to partner with FairPlay? </strong> </span><br />
+                    <textarea className="form-control" name="describePartnerRelationship" rows="3" placeholder="Brief description." value={director_profile.describePartnerRelationship} onChange={onChange}></textarea>
+                  
+                   
                   </div>
-                  <div className="col-md-3 mb-2">
-                    <button type="button" class="btn btn-outline-secondary"> Clear Form</button>
-                    </div>
-                    <div className="col-md-3  offset-md-2">
-                  <button type="button" class= "btn btn-primary" id="submitButton"> Submit For Review</button>
+                  
+                  <br />
                 </div>
-                </div>
-                
-                </div>
-                <br />
+
               </div>
-              <br />
-              <br />
-              <br />
-            </div>
+
+              
+              <button className="btn btn-primary" type="submit">Submit</button>
+          </form>
           </div>
-        </main>
-        ); 
-      } 
+          
+          <p>{JSON.stringify(director_profile)}</p>
+      </main>
+  );
+}
 export default Settings;
