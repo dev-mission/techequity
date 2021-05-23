dd73b1b8c69fcd9e8e9e4952f5f73eb5695d6b12
Zizo Ezzat <ziezzat2002@gmail.com>
2021-05-22 22:50:43 -0700

    added small change

--- client/src/Setup/ProgramDirector.js
+++ client/src/Setup/ProgramDirector.js
@@ -40,33 +40,6 @@ function ProgramDirector() {
       console.log(error);
     }
   }
-<<<<<<< HEAD
-  function resetForm(form){
-      var inputs = form.getElementsByTagName('input');
-      for (var i = 0; i<inputs.length; i++) {
-        switch (inputs[i].type) {
-            // case 'hidden':
-            case 'text':
-                inputs[i].value = '';
-                break;
-            case 'radio':
-            case 'checkbox':
-                inputs[i].checked = false;   
-        }
-    }
-    var selects = form.getElementsByTagName('select');
-    for (var i = 0; i<selects.length; i++)
-        selects[i].selectedIndex = 0;
-
-    // clearing textarea
-    var text= form.getElementsByTagName('textarea');
-    for (var i = 0; i<text.length; i++)
-        text[i].innerHTML= '';
-
-    return false;
-  }
-=======
->>>>>>> main
 
   return (
     <main className="container">
