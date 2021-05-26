f1e0b17ca50ca4309be7650046adebe63f606018
andres <andresalexmartin@gmail.com>
2021-05-25 18:37:59 -0700

    creating the look of the donor sign up and bugs

--- client/src/Setup/Donor.js
+++ client/src/Setup/Donor.js
@@ -8,16 +8,18 @@ import { StatusCodes } from 'http-status-codes'
 import Api from '../Api';
 import UnexpectedError from '../UnexpectedError';
 import ValidationError from '../ValidationError';
-import '../Register.scss';
+import '../Donor.scss';
 //test imports
 function Donor() {
   const { donor } = useAuthContext(); // taken out for donor already defined issue
   //test copy
- 
-  const authContext = useAuthContext();  
+
+  const authContext = useAuthContext();
   const history = useHistory();
+  const [user, setUser] = useState('donor')
   const [role, setRole] = useState('donor');  // removed for now cause i think its only necessary when registering
-   const [donorstate, setDonorstate] = useState({
+  const [donorState, setDonorstate] = useState({
+    firstName: `${user.firstName}`,
     donorType: '',
     userRole: '',
     OrganizationId: '',
@@ -54,17 +56,49 @@ function Donor() {
 
   return (
     <main>
-      <h1>Welcome, {donor && donor.firstName}! (Donor)</h1>
+      
+      <div>
+        <div id="innerbox">
 
-      <form onSubmit={onSubmit}>
-        {error && error.message && (
-          <div className="alert alert-danger">{error.message}</div>
-        )}
-      </form>
-      <div className="col-md-6">
-        <label for="donorType"> <strong>donor type</strong></label>
-        <input type="text" class={classNames('form-control', { 'is-invalid': error?.errorsFor?.('donortype') })} id="donortype" name="donortype" onChange={onChange} value={donor.donorType} placeholder="donortype"></input>
-        {error?.errorMessagesHTMLFor?.('donorType')}
+          
+        <h1>Welcome, {donorState.firstName}! (Donor) <br></br> Let's finish setting up your donor profile.</h1>
+
+          <form onSubmit={onSubmit}>
+            {error && error.message && (
+              <div className="alert alert-danger">{error.message}</div>
+            )}
+          </form>
+
+          
+            <div>
+              <div>
+                <label for="donorType"> <strong>donor type</strong></label>
+                <input type="text" class={classNames('form-control', { 'is-invalid': error?.errorsFor?.('donorType') })} id="donorType" name="donorType" onChange={onChange} value={donorState.donorType} placeholder="Donor"></input>
+                {error?.errorMessagesHTMLFor?.('donorType')}
+              </div>
+              <div>
+                <label for="userRole"> <strong>user role</strong></label>
+                <input type="text" class={classNames('form-control', { 'is-invalid': error?.errorsFor?.('userRole') })} id="userRole" name="userRole" onChange={onChange} value={donorState.userRole} placeholder="Role"></input>
+                {error?.errorMessagesHTMLFor?.('userRole')}
+              </div>
+              <div>
+                <label for="webLink"> <strong>web link</strong></label>
+                <input type="text" class={classNames('form-control', { 'is-invalid': error?.errorsFor?.('webLink') })} id="webLink" name="webLink" onChange={onChange} value={donorState.webLink} placeholder="Link"></input>
+                {error?.errorMessagesHTMLFor?.('webLink')}
+              </div>
+              <div>
+                <label for="heardAboutUs"> <strong>How have you heard about us</strong></label>
+                <input type="text" class={classNames('form-control', { 'is-invalid': error?.errorsFor?.('heardAboutUs') })} id="heardAboutUs" name="heardAboutUs" onChange={onChange} value={donorState.heardAboutUs} placeholder="heard aout us"></input>
+                {error?.errorMessagesHTMLFor?.('heardAboutUs')}
+              </div>
+              <div>
+                <label for="MissionVision"> <strong>Mission/Vision statement</strong></label>
+                <input type="text" class={classNames('form-control', { 'is-invalid': error?.errorsFor?.('missionVision') })} id="missionVision" name="missionVision" onChange={onChange} value={donorState.missionVision} placeholder="statement"></input>
+                {error?.errorMessagesHTMLFor?.('missionVision')}
+              </div>
+            </div>
+          
+        </div>
       </div>
 
     </main>
