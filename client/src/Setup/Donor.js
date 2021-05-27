3b273deecf287b3deb80e0e7deee96e591c67706
andres <andresalexmartin@gmail.com>
2021-05-27 15:45:50 -0700

    got the donor form looking presentable

--- client/src/Setup/Donor.js
+++ client/src/Setup/Donor.js
@@ -53,54 +53,69 @@ function Donor() {
   }
   //test copy
 
-//undo marker 
+  //undo marker 
   return (
     <main className="container">
-      
-      
-        <div>
+      <h1 className="heading">Welcome, {donorState.firstName}! (Donor)</h1>
+      <p className="description"> Let's finish setting up your donor profile.</p>
+      <form className="form" onSubmit={onSubmit}>
 
-          
-        <h1 className="heading">Welcome, {donorState.firstName}! (Donor)</h1>
-        <p className="description"> Let's finish setting up your donor profile.</p>
+        {/*    {error && error.message && (
+          <div className="alert alert-danger">{error.message}</div>
+        )}    */}
 
-          <form className="form" onSubmit={onSubmit}>
-            {error && error.message && (
-              <div className="alert alert-danger">{error.message}</div>
-            )}
-          </form>
-
-          
-            <div>
-              <div>
-                <label for="donorType"> <strong>donor type</strong></label>
-                <input type="text" class={classNames('form-control', { 'is-invalid': error?.errorsFor?.('donorType') })} id="donorType" name="donorType" onChange={onChange} value={donorState.donorType} placeholder="Donor"></input>
+        <div className="row">
+          <div className="col-md-3 offset-1">
+            <p className="requiredResponse">* Indicates a required response</p>
+          </div>
+          {/*questions*/}
+          <div>
+            <div className="col-md-5 offset-4">
+              <div className="form-group">
+                <label for="donorType"> <label className="requiredResponse" >* </label><strong>donor type</strong></label>
+                <input type="text" placeholder=":D" class={classNames('form-control', { 'is-invalid': error?.errorsFor?.('donorType') })} id="donorType" name="donorType" onChange={onChange} value={donorState.donorType} placeholder="Donor"></input>
                 {error?.errorMessagesHTMLFor?.('donorType')}
               </div>
-              <div>
-                <label for="userRole"> <strong>user role</strong></label>
+            </div>
+            <div className="col-md-5 offset-4">
+              <div className="form-group">
+                <label for="userRole"> <label className="requiredResponse" >* </label><strong>user role</strong></label>
                 <input type="text" class={classNames('form-control', { 'is-invalid': error?.errorsFor?.('userRole') })} id="userRole" name="userRole" onChange={onChange} value={donorState.userRole} placeholder="Role"></input>
                 {error?.errorMessagesHTMLFor?.('userRole')}
               </div>
-              <div>
-                <label for="webLink"> <strong>web link</strong></label>
+            </div>
+            <div className="col-md-5 offset-4">
+              <div className="form-group">
+                <label for="webLink"> <label className="requiredResponse" >* </label><strong>web link</strong></label>
                 <input type="text" class={classNames('form-control', { 'is-invalid': error?.errorsFor?.('webLink') })} id="webLink" name="webLink" onChange={onChange} value={donorState.webLink} placeholder="Link"></input>
                 {error?.errorMessagesHTMLFor?.('webLink')}
               </div>
-              <div>
-                <label for="heardAboutUs"> <strong>How have you heard about us</strong></label>
+            </div>
+            <div className="col-md-5 offset-4">
+              <div className="form-group">
+                <label for="heardAboutUs"> <label className="requiredResponse" >* </label><strong>How have you heard about us</strong></label>
                 <input type="text" class={classNames('form-control', { 'is-invalid': error?.errorsFor?.('heardAboutUs') })} id="heardAboutUs" name="heardAboutUs" onChange={onChange} value={donorState.heardAboutUs} placeholder="heard aout us"></input>
                 {error?.errorMessagesHTMLFor?.('heardAboutUs')}
               </div>
-              <div>
-                <label for="MissionVision"> <strong>Mission/Vision statement</strong></label>
+            </div>
+            <div className="col-md-5 offset-4">
+              <div className="form-group">
+                <label for="MissionVision"> <label className="requiredResponse" >* </label><strong>Mission/Vision statement</strong></label>
                 <input type="text" class={classNames('form-control', { 'is-invalid': error?.errorsFor?.('missionVision') })} id="missionVision" name="missionVision" onChange={onChange} value={donorState.missionVision} placeholder="statement"></input>
                 {error?.errorMessagesHTMLFor?.('missionVision')}
               </div>
             </div>
-          
+          </div>
+
+
         </div>
-      
+        <div className="row">
+          <div className="offset-9">
+            <button className="btn btn-primary" type="submit">Submit</button>
+          </div>
+        </div>
+      </form>
+      <p>{JSON.stringify(donor)}</p>
     </main>
   );
 }
